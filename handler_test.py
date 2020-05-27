64ed2a4ce9e7bb2d75d187cdf5c49bbe61a631c5
rs921 <yifeng.zhong@cummins.com>
2020-05-26 23:16:09 -0400

    fix unittest

--- handler_test.py
+++ handler_test.py
@@ -5,11 +5,17 @@ def test_handler():
     event = {
         "httpMethod": "POST",
         "path": "/cp/sharelatlong",
-        "body": '{"hello":"1"}'
+        "body": '{"hello":"1"}',
+        "headers": { 
+            "Access-Control-Allow-Origin": "*" 
+        } 
     }
     result = handler.hello(event, None)
     assert result == {
         "statusCode": 200,
-        "body": '{"message": "Hello World!!"}'
+        "body": '{"message": "Hello World!!"}',
+        "headers": { 
+            "Access-Control-Allow-Origin": "*" 
+        } 
     }
 
