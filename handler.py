e8f64f50c1cd72040b898dbe761173f07b6e4feb
rs921 <yifeng.zhong@cummins.com>
2020-05-27 00:33:52 -0400

    commit

--- handler.py
+++ handler.py
@@ -20,9 +20,9 @@ def hello(event, context):
     response = {
         "statusCode": 200,
         "body": json.dumps(body),
-        "headers": { 
-            "Access-Control-Allow-Origin": "*" 
-        }        
+        # "headers": { 
+        #     "Access-Control-Allow-Origin": "*" 
+        # }        
     }
     print("hello")
     return response
